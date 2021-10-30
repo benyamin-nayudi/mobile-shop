956235128f1f213bc5943a607159a39f83df25f7
benyamin-nayudi <benyamin.naudi@gmail.com>
2021-10-30 09:22:16 +0330

    backend api initilization

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/' , (req , res)=>{
+    res.send('welcome to our mobile shop api ...')
+})
+
+app.get('/products' , (req , res)=>{
+    res.send([2,3,4])
+})
+
+
+const port = process.env.PORT || 5000 ;
+app.listen(port , console.log('server running on port ' + port ))
+
